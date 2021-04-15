3bdefba0cd2eee31b19b4f2f6a432ec614b0e64f
Rados13 <radoslawszuma@gmail.com>
2021-04-15 11:55:29 +0200

    Create bash script to start configuration

---
+++ prepare.sh
@@ -0,0 +1,13 @@
+
+#Prepare virtual interface
+sudo iw phy phy0 interface add hotspot type __ap
+sudo ifconfig hotspot 192.168.28.1
+
+# Install packages needed for taking photos with python-opencv
+sudo apt-get install libatlas-base-dev
+
+# Run hotspot
+#sudo hostapd hostapd.conf
+
+# Run dhcp server
+# sudo udhcpd -f
