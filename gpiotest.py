4cff1ae0940e23e6462216b070ea79a13cb6e4fc
Radoslaw Szuma <radoslawszuma@gmail.com>
2021-05-12 17:37:30 +0100

    Add python package network

---
+++ gpiotest.py
@@ -0,0 +1,28 @@
+
+from gpiozero import Button
+from time import sleep
+
+button = Button(26)
+
+while True:
+    if button.is_pressed:
+        print("Pressed")
+    else:
+        print("Released")
+
+
+    sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
