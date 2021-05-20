8c6238e620d9fda2388fc2ec879903880f142ef1
Rados13 <radoslawszuma@gmail.com>
2021-05-20 19:55:31 +0200

    Fix quotes

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def button_action():
     print(f"Start meeting at {link}")
 
     def func():
-        command = f'''python3 -c "import jitsi_connection;jitsi_connection.open_page('{link}')" '''
+        command = f"python3 -c '''import jitsi_connection;jitsi_connection.open_page('{link}')'''"
         os.system(f'''su - pi -c "{command}"''')
 
     callThread = Process(target=func)
