f4fe2dc4d77d5df7a0034fc98f3b53d082204d53
Rados13 <radoslawszuma@gmail.com>
2021-05-20 19:51:25 +0200

    Another try with runnging scripts as user

--- main.py
+++ main.py
@@ -38,7 +38,8 @@ def button_action():
     print(f"Start meeting at {link}")
 
     def func():
-        os.system(f'''python3 -c "import jitsi_connection;jitsi_connection.open_page('{link}')"''')
+        command = f'''python3 -c "import jitsi_connection;jitsi_connection.open_page('{link}')" '''
+        os.system(f'''su - pi -c "{command}"''')
 
     callThread = Process(target=func)
     callThread.start()
