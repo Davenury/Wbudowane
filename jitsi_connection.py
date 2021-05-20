91aadc0fcac75d37760bfe4f725262a953b611e1
Rados13 <radoslawszuma@gmail.com>
2021-05-20 16:41:39 +0200

    Add port for selenium

--- jitsi_connection.py
+++ jitsi_connection.py
@@ -12,6 +12,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("headless")
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chromeOptions.add_argument("--remote-debugging-port=9222")
 chrome_options.add_experimental_option("prefs", {"profile.default_content_setting_values.media_stream_mic": 1,
                                                  "profile.default_content_setting_values.media_stream_camera": 1,
                                                  "profile.default_content_setting_values.geolocation": 1,
