157807734d744ede3e64e60ad9d2250ebc308bc6
Rados13 <radoslawszuma@gmail.com>
2021-05-22 12:01:19 +0200

    Another changes

--- jitsi_connection.py
+++ jitsi_connection.py
@@ -13,11 +13,13 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("headless")
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument("--remote-debugging-port=9222")
+#chrome_options.add_argument("--remote-debugging-port=9222")
 chrome_options.add_experimental_option("prefs", {"profile.default_content_setting_values.media_stream_mic": 1,
                                                  "profile.default_content_setting_values.media_stream_camera": 1,
                                                  "profile.default_content_setting_values.geolocation": 1,
-                                                 "profile.default_content_setting_values.notifications": 1
+                                                 "profile.default_content_setting_values.notifications": 1,
+                                                 "hardware.audio_capture_enabled":True,
+                                                 "hardware.video_capture_enabled":True
                                                  })
 chrome_options.add_argument("--use-fake-ui-for-media-stream=1")
 
